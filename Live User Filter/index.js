dca8cc45226316250d41e54f1e145f14e2c6ca8c
Ashutosh parihar <ashucool049@gmail.com>
2023-01-08 19:21:58 +0530

    hey

--- Live User Filter/index.js
+++ Live User Filter/index.js
@@ -1,1 +1,32 @@
-console.log('heyyyy');
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+const listitems = [];
+
+
+filter.addEventListener('input',(e)=>{
+    filterdata(e.target.value); 
+});
+
+function getdata(){
+    fetch('https://randomuser.me/api/?results=50').then(res=>res.json()).then(data=>{
+        result.innerHTML = '';
+        data.results.forEach(user => {
+            const li = document.createElement('li');
+            listitems.push(li);
+            li.innerHTML = `<img src='${user.picture.large}'>
+            <div class='user-info> <h4>${user.name.first} </div>`;
+            result.appendChild(li)
+        });
+    });
+}
+
+function filterdata(searchterm){
+    listitems.forEach(item =>{
+        if(item.toLowerCase().includes(searchterm.toLowerCase())){
+            item.classList.remove('hide')
+        }
+        else{
+            item.classList.add('hide')
+        }
+    });
+}
