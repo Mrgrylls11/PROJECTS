9aed0c4f2e5df74b18d9d1391e43790d225d561b
Ashutosh <ashucool049@gmail.com>
2023-03-23 11:39:05 +0530

    hi

--- TO DO LIST/Index.js
+++ TO DO LIST/Index.js
@@ -5,23 +5,51 @@ const mybtn = document.querySelector('.btn');
 const mylist = document.querySelector('.ulclass');
 
 
-mybtn.addEventListener('click',()=>{
-    const textval = myinput.value;
+mybtn.addEventListener('click', () => {
+    // this is the original value from the input
+    const textval = myinput.value;    
+
+    //created the new li tag
     const newele = document.createElement('li');
 
-    const newtotal = document.createElement('button');
-    newtotal.innerText = 'x';
+    //li tag with the input value
+    newele.innerText = textval;
+    console.log(textval);
+
+    
+
+    //created the button for delete
     const cncl = document.createElement('button');
-    cncl.textContent = 'X';
-    newele.innerHTML = `${textval} + ${cncl}`;
+    cncl.innerText = "x";
+    cncl.addEventListener('click', removemyele);
+
+    newele.append(cncl);
+
+    //inserting the new element inside the main unordered list
     mylist.append(newele);
 
-
+    //created the inner value of the delete button
+    cncl.innerText = 'X';
 
     myinput.value = "";
-})
-myinput.addEventListener('keypress',(event)=>{
-    if(event.key === 'Enter'){
-        mybtn.click();
+}
+
+)
+//eventlistener for input element to peroform click task on enter after typing
+myinput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        if (myinput.value == "") {
+            alert('no input given')
+        }
+        else {
+            mybtn.click();
+        }
     }
-})
+});
+
+//function for delete button click event
+function removemyele(task){
+    console.log('delete button is clicked', task);
+    task.target.parentElement.remove()
+}
+
