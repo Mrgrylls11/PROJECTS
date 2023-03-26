fd3c1963c0240e2f2b633e5d65fb7a369b26000d
Ashutosh <ashucool049@gmail.com>
2023-03-26 18:31:18 +0530

    hey

--- Counter App/index.js
+++ Counter App/index.js
@@ -1,27 +1,47 @@
 
 const postvbtn = document.querySelector('.positive');
 const negtvbtn = document.querySelector('.negative');
-const dispnum = document.querySelector('.display');
+var dispnum = document.querySelector('.display');
 const resetbtn = document.querySelector('#reset');
-
+var count = 0;
 
 //positive button event listener
-postvbtn.addEventListener('click', funn);
-function funn() {
-    // alert("stop")
-    console.log('button clicked');
-}
+postvbtn.addEventListener('click', () => {
+    // console.log('button clicked');
+    if(dispnum.innerHTML === 'Start'){
+        count++
+    }
+    else if(dispnum.innerHTML <= 0){
+        count++
+    }
+    else{
+        count++
+    }
+    dispnum.innerHTML = count;
+    console.log(dispnum.innerHTML);
+});
 
 
 
 //negative button event listener
 negtvbtn.addEventListener('click', () => {
-    console.log('negative btn');
+    // console.log('negative btn');
+    if(dispnum.innerHTML === '0' || dispnum.innerHTML === 'Start'){
+        alert('number can\'t be less than 0');
+        location.reload();
+    }
+    else{
+        count--;
+    }
+    dispnum.innerHTML = count;
 });
 
 
 
 //reset button event listener
 resetbtn.addEventListener('click', () => {
-    console.log('reset btn');
+    count = 0;
+    dispnum.innerHTML = count;
+    // location.reload();
 })
+
