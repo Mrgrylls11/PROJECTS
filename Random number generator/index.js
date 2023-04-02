042789d8442152840dab7942cf1fd0031ea163cc
Ashutosh <ashucool049@gmail.com>
2023-04-02 20:28:56 +0530

    hi

--- Random number generator/index.js
+++ Random number generator/index.js
@@ -8,9 +8,9 @@ mybtn.addEventListener('click', () => {
     console.log(myrandom);
     dispnum.innerHTML = `${myrandom}`;
 })
-// mybtn.addEventListener('click', setInterval(myfun,100));
+mybtn.addEventListener('click', setInterval(myfun,50));
 function myfun(){
     var randoom = Math.random().toString(10).slice(2, 3);
-    console.log(randoom); 
+    // console.log(randoom); 
     spann.innerHTML = `${randoom}`
 }
