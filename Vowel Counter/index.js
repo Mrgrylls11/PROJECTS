02a1565eaee4e4f7f05728f35b8846bcf62e0662
Ashutosh <ashucool049@gmail.com>
2023-04-21 19:30:22 +0530

    hi

--- Vowel Counter/index.js
+++ Vowel Counter/index.js
@@ -4,8 +4,26 @@ const btninp = document.querySelector('.btn');
 
 
 btninp.addEventListener('click', () => {
-    if(inptext.value === ''){
-        alert('input can not be left empty, give input.')
+    //To check if the input field is empty or not.
+    if (inptext.value === '') {
+        alert('Input can not be left empty, give input.')
+    }
+    let myval = inptext.value;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    var vcount = 0;
+    var aryy = [];
+
+    for (let char of myval) {
+        if (vowels.includes(char)) {
+            vcount++
+            aryy.push(char)
+        }
     }
     
+
+    disptext.innerHTML = `There are ${vcount} number of vowels which are '${aryy}'`
+
+    //value after every interval set to null.
+    inptext.value = null;
 })
