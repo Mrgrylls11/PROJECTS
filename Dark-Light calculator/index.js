45628b621f45d2b3117f8c4677c37af2620c7169
Ashutosh <ashucool049@gmail.com>
2023-05-04 17:15:25 +0530

    hi

--- Dark-Light calculator/index.js
+++ Dark-Light calculator/index.js
@@ -1,27 +1,12 @@
-// let darkmode = localStorage.getItem('darkmode')
-// let toggler = document.querySelector('.toggler');
-// const disptext = document.querySelector('.disptext');
-// const mybtn = document.querySelector('td');
 
-// const enableDarkMode = ()=>{
-//     localStorage.setItem('darkmode', 'enabled')
-// }
-
-// toggler.addEventListener('click',()=>{
-//     console.log('test');
-// })
-
-// mybtn.addEventListener('click',()=>{
-//     console.log('hey');
-// })
 const togglerbtn = document.querySelector('.toggler');
 const themedark = document.querySelector('.dark');
 
-let ifdark = true;
-
-togglerbtn.onclick = () =>{
+// let ifdark = true;
+// togglerbtn.onclick = () => myfunction;
+togglerbtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('.dark');
     console.log('hey');
-    themedark.classList.toggle("dark");
-    togglerbtn.classList.toggle("active");
-    ifdark = !ifdark;
-};
+})
+
