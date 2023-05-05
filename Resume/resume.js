ad8ab79da59123ead9e8e6647a63276fc2b955a5
Ashutosh <ashucool049@gmail.com>
2023-05-06 01:15:30 +0530

    hi

--- Resume/resume.js
+++ Resume/resume.js
@@ -1,6 +1,13 @@
-const copying = document.querySelector('.myemail');
-
+const copying = document.querySelector('.one');
+let tag = document.querySelector('.here');
+tag.style.display = "none";
 copying.addEventListener('click',()=>{
+
     navigator.clipboard.writeText("ashucool049@gmail.com");
-    console.log('hey');
+    // console.log('hey');
+    
+    tag.style.display = "block";
+  setTimeout(function() {
+    tag.style.display = "none";
+  }, 200);
 })
