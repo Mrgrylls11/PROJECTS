b96ba73b3e02d98929e634982926bee8e4b616f8
Ashutosh <ashucool049@gmail.com>
2023-05-08 20:38:06 +0530

    hiii

--- Dark-Light calculator/index.js
+++ Dark-Light calculator/index.js
@@ -1,12 +1,32 @@
 
 const togglerbtn = document.querySelector('.toggler');
-const themedark = document.querySelector('.dark');
+const clss = document.querySelector('.darkmode');
+togglerbtn.addEventListener('change', () => {
+    document.body.classList.toggle('darkmode');
+});
+const inputbtn = document.querySelectorAll("button");
+const disptext = document.querySelector('.disptext');
 
-// let ifdark = true;
-// togglerbtn.onclick = () => myfunction;
-togglerbtn.addEventListener('click', () => {
-    let element = document.body;
-    element.classList.toggle('.dark');
-    console.log('hey');
-})
+inputbtn.forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.value == 'clear') {
+            disptext.innerHTML = '';
+        }
+        else if (element.value == 'erase'){
+            var text = disptext.innerHTML.toString();
+            disptext.innerHTML = string.substring(0,string.length-1);
+        }
+        else if(element.value == '(' || element.value == ')'){
+
+            disptext.innerHTML = disptext.innerHTML+element.value;
+        }
+        else if(element.value == '='){
+            disptext.innerHTML = eval(disptext.innerHTML)
+        }
+        else{
+            disptext.innerHTML +=element.value;
+        }
+
+    })
+});
 
