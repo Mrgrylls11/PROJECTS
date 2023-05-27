61ee5f7687c53bbc927d6b994b239302b7b3f81c
Ashutosh <ashucool049@gmail.com>
2023-05-27 12:34:07 +0530

    hi

---
+++ Random Joke/index.js
@@ -0,0 +1,21 @@
+const getbtn = document.querySelector('.getbtn');
+const joke1 = document.querySelector('.joke');
+const joke2 = document.querySelector('.joke2');
+const categ = document.querySelector('.category');
+
+getbtn.addEventListener('click', () => {
+    const jokedata = fetch("https://v2.jokeapi.dev/joke/any");
+    const maindata = jokedata.then((response) => {
+        return response.json();
+    });
+    maindata.then((result) => {
+        categ.innerHTML = `Type:` + result.category
+        if (result.type == 'single') {
+            joke1.innerHTML = result.joke
+        }
+        else {
+            joke1.innerHTML = result.setup
+            joke2.innerHTML = result.delivery
+        }
+    });
+});
