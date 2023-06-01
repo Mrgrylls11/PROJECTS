82cd22b6a109996bc767df6a7c491c911e60d30e
Ashutosh <ashucool049@gmail.com>
2023-06-01 21:53:27 +0530

    hi

---
+++ Digital Watch/watch.js
@@ -0,0 +1,21 @@
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+const bttn = document.querySelector('.fullscreen')
+const mycontainer = document.querySelector('.main')
+function myfun() {
+    const time = new Date();
+    let myhours = time.getHours();
+    let myminutes = time.getMinutes();
+    let myseconds = time.getSeconds();
+    hours.innerHTML = `${Math.floor(myhours / 2)} <div>HOURS</div>`
+    minutes.innerHTML = `${myminutes} <div>MINUTES</div>`
+    seconds.innerHTML = `${myseconds} <div>SECONDS</div>`
+}
+bttn.addEventListener('click', () => {
+    if (mycontainer.requestFullscreen) {
+        mycontainer.requestFullscreen();
+    }
+})
+
+setInterval(myfun, 16);
