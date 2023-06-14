d3f181c0908a4abde4befd96ebfccccd22d7a1cc
Ashutosh <ashucool049@gmail.com>
2023-06-15 00:37:50 +0530

    hi

--- Weather App/code.js
+++ Weather App/code.js
@@ -1,7 +1,3 @@
-let myApi = {
-    key: '24d26e86cc84b567c2140ceb9c8a7861',
-    proxy: 'https://cors-anywhere.herokuapp.com/'
-}
 //getting all the DOM elements
 let cityname = document.querySelector('h1');
 let countryname = document.querySelector('h3');
@@ -10,48 +6,42 @@ let humidity = document.querySelector('.humidityp');
 let temperature = document.querySelector('.tempp');
 
 
-const weather = {};
-weather.temperature = {
-    unit: 'celcius'
+//setting the api key and proxy server for cors error handeling.
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+
+
+
+//getting coordinates
+function coordinates(position) {
+    let lat = position.coords.latitude.toString().slice(0, 5);
+    let long = position.coords.longitude.toString().slice(0, 5);
+    // console.log(lat,long);
+    currentweather(lat, long)
 }
 
-weather.temprature = {
-    unit: 'celcius'
+//fetching the weather by the API
+async function currentweather(latitude, longitude) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    //putting all details in the display
+    let icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    cityname.innerHTML = data.name;
+    countryname.innerHTML = data.sys.country;
+    image.src = icon;
+    humidity.innerHTML = data.main.humidity;
+    temperature.innerHTML = `${data.main.temp.toFixed()}°C`;
+
 }
-//checking if the location is granted or not.
+currentweather();
+
+//gpn on checking
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(setposition, showerr)
+    navigator.geolocation.getCurrentPosition(coordinates, errfun);
 }
-
-//getting the coordinates
-function setposition(position) {
-    var mylatitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    getweather(mylatitude, longitude)
-}
-
-function getweather(mylat, mylong) {
-    let link = `${myApi.proxy}https://api.openweathermap.org/data/3.0/onecall?lat=${mylat}&lon=${mylong}&appid=${myApi.key}`;
-    fetch(link)  //send a network request and get the information from the server
-        .then(response => {
-            let data = response.json(); //now whatever response we get, parse it into json and store in data
-            return data;
-        })
-        .then(data => { //then we update our weather object with the required data 
-            console.log(data);
-            // weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            // weather.description = data.weather[0].description;
-            // weather.iconID = data.weather[0].icon;
-            // weather.city = data.name;
-            // weather.country = data.sys.country;
-            // weather.humidity = data.main.humidity;
-        })
-        .then(() => { //then display the weather to the ui
-            displayWeather();
-        });
-}
-
-//error for location turned off
-function showerr() {
-    console.log('location is not on');
+//if gps off then error
+function errfun() {
+    console.log('location on kar');
+    alert('gps is off please turn it on');
 }
